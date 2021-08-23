574fd1aceb38ee7cea8ad35737a7f9e4edb57953
Amater444su <323.5432@gmail.com>
2021-08-23 04:32:42 +0300

    Added test to TodoDetailView endpoint

--- todo/tests/test_models.py
+++ todo/tests/test_models.py
@@ -1,8 +1,11 @@
-
-
-
-
-class TestModels:
-
-    def test_todo_exist(self):
-        pass
+# from django.db.models import Count
+# from todo.models import *
+# import pytest
+#
+#
+# @pytest.mark.django_db
+# class TestModels:
+#
+#     def test_todo_exist(self):
+#         todo = Todo.objects.annotate(todo_count=Count('title'))
+#         assert todo['todo_count'] > 0
--- todo/tests/test_views.py
+++ todo/tests/test_views.py
@@ -1,18 +1,40 @@
+import ipdb
 from django import urls
 from django.contrib.auth import get_user_model
 import pytest
+from django.contrib.auth.models import AnonymousUser
+from mixer.backend.django import mixer
+from django.test import RequestFactory
+from todo.models import Profile
+from todo.views import TodoDetailView
 
 
 @pytest.mark.django_db
-@pytest.mark.parametrize('param', [
-    ('todo')
-])
-def test_render_views(client, param):
-    # client (instance django test client) can simulate post and get requests
-    temp_url = urls.reverse(param)
-    response = client.get(temp_url)
-    assert response.status_code == 200
+class TestViews:
 
+    @pytest.mark.parametrize('param', [
+        ('todo')
+    ])
+    def test_render_views(self, param, client):
+        # client (instance django test client) can simulate post and get requests
+        temp_url = urls.reverse(param)
+        response = client.get(temp_url)
+        assert response.status_code == 200
 
-def test_user_create_todo(client):
-    pass
+    def test_todo_detail_authenticated(self):
+        mixer.blend('todo.Todo')
+        path = urls.reverse('todo-edit', kwargs={'pk': 1})
+        request = RequestFactory().get(path)
+        request.user = mixer.blend(Profile)
+
+        response = TodoDetailView.as_view()(request, pk=1)
+        assert response.status_code == 200
+
+    def test_todo_detail_unauthenticated(self):
+        mixer.blend('todo.Todo')
+        path = urls.reverse('todo-edit', kwargs={'pk': 1})
+        request = RequestFactory().get(path)
+        request.user = AnonymousUser()
+
+        response = TodoDetailView.as_view()(request, pk=1)
+        assert response.status_code == 200
