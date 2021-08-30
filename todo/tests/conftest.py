5371c978d06e97c10d4088789014da06b5862b53
Amater444su <323.5432@gmail.com>
2021-08-30 18:37:36 +0300

    Added test to all todo endpoints

--- todo/tests/conftest.py
+++ todo/tests/conftest.py
@@ -21,19 +21,9 @@ def user():
     return UserFactory()
 
 
-# @pytest.fixture
-# def api_client():
-#     return APIClient()
-
-
 @pytest.fixture
 def api_client_authenticated(db, user):
     api_client = APIClient()
     api_client.force_authenticate(user=user)
     yield api_client
     api_client.force_authenticate(user=None)
-
-
-# @pytest.fixture
-# def task() -> Todo:
-#     return Todo.objects.create(title='test_title', description='test_description')
--- todo/tests/test_views.py
+++ todo/tests/test_views.py
@@ -7,7 +7,7 @@ from django.test import RequestFactory
 from rest_framework.test import APIClient
 from django.db import transaction
 
-from todo.models import Profile, Todo
+from todo.models import Profile, Todo, Comments
 from todo.tests.factories import TodoFactory
 from todo.views import TodoDetailView
 
@@ -43,25 +43,44 @@ class TestTodoViews:
 
         assert response.status_code == 403
 
-    # def test_list_todo(self, api_client_authenticated, user):
-    #     assert Todo.objects.count() == 0
-    #     todo = Todo.objects.create(
-    #         title='test_todo',
-    #         author=user,
-    #     )
-    #     response = api_client_authenticated.get('')
-    #
-    #     assert response.status_code == 200
-    #     assert response.json() == [
-    #         {
-    #             'id': todo.id,
-    #             'title': 'test_todo',
-    #             'author': user.id,
-    #             'description': None,
-    #             'categories': None,
-    #             'todo_comment': []
-    #         }
-    #     ]
+    def test_list_todo(self, api_client_authenticated, user, todo):
+        todo.author = user
+        todo.save()
+        path = reverse('todo')
+        response = api_client_authenticated.get(path)
+        # ipdb.set_trace()
+        assert response.status_code == 200
+        assert Todo.objects.count() == 1
+        assert response.json() == [
+            {
+                'id': todo.id,
+                'title': todo.title,
+                'author': user.username,
+                'description': None,
+                'categories': None,
+                'todo_comment': []
+            }
+        ]
+
+    def test_create_comment_for_todo(self, api_client_authenticated, todo):
+        assert Comments.objects.count() == 0
+
+        path = reverse('todo-comment', kwargs={'todo_id': todo.id})
+        request_data = {
+            'text': 'test_text'
+        }
+        response = api_client_authenticated.post(path, request_data)
+        comment = Comments.objects.get(id=1)
+
+        assert response.status_code == 201
+        assert Comments.objects.count() == 1
+        # ipdb.set_trace()
+        assert response.json() == {
+
+                'id': comment.id,
+                'text': comment.text
+            }
+
 
 
 class TestGroupView:
